a2715f8b224d5993209503f619a1518670fa1480
fengyinglouzz <623385757@qq.com>
2019-06-23 00:07:53 +0800

    nothing

--- center.py
+++ center.py
@@ -5,14 +5,15 @@ if __name__ == '__main__':
     for i in range(1, 10):
         start = "18-12-0" + str(i) + "-12"
         end = "18-12-0" + str(i) + "-14"
-        spiderman.run("经历", start, end, "经历" + str(i))
+        spiderman.run("四川 平安", start, end, "四川 平安" + str(i))
     for i in range(10, 32):
         start = "18-12-" + str(i) + "-12"
         end = "18-12-" + str(i) + "-14"
-        spiderman.run("经历", start, end, "经历" + str(i))
+        spiderman.run("四川 平安", start, end, "四川 平安" + str(i))
 
     # countList = []
-    # for i in range(1, 19):
-    #     filename = "results/四川" + str(i) + ".txt"
-    #     countList.append(countLine.run(filename))
+    # for i in range(1, 18):
+    #     filename = "results/平安" + str(i) + ".txt"
+    #     substr = '%i'%(i) + ':%s' %(countLine.run(filename))
+    #     countList.append(substr)
     # print(countList)
